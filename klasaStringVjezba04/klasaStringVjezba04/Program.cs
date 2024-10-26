79b5cfcfcf4c0c2bfc432e0d615ce11a775d61ac
DavidCuturic5 <david.cuturic5@gmail.com>
2024-10-26 21:59:31 +0200

    Add files via upload

---
+++ klasaStringVjezba04/klasaStringVjezba04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaStringVjezba04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite rečenicu:");
+            string unos = Console.ReadLine();
+
+            string[] riječi = unos.Split(' ');
+            string prvariječ = riječi[0];
+            string zadnjariječ = riječi[riječi.Length - 1];
+
+            Console.WriteLine("Prva riječ je:" + prvariječ);
+            Console.WriteLine("Zadnja riječ je:" + zadnjariječ);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
